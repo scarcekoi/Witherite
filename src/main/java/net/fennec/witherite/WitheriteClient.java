3738c8ae6c118e88c297f79d27d2eb173941824a
CuteFox8302 <theloniousingramprice@gmail.com>
2023-12-31 20:53:38 -0500

    Initial commit

---
+++ src/main/java/net/fennec/witherite/WitheriteClient.java
@@ -0,0 +1,10 @@
+package net.fennec.witherite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WitheriteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/fennec/witherite/WitheriteDataGenerator.java
@@ -0,0 +1,11 @@
+package net.fennec.witherite;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WitheriteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
